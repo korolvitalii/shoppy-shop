b0a9d1914845841e0fbf4fd7b66596c299ed2e68
korolvitalii <korolvitalik@gmail.com>
2026-07-05 11:29:00 +0100

    feat(checkout): add accessible progress stepper

---
+++ src/app/features/checkout/components/checkout-stepper/checkout-stepper.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-checkout-stepper',
+  imports: [RouterLink],
+  templateUrl: './checkout-stepper.html',
+  styleUrl: './checkout-stepper.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CheckoutStepper {
+  readonly currentStep = input.required<1 | 2 | 3>();
+  readonly deliveryComplete = input(false);
+  readonly paymentComplete = input(false);
+}
